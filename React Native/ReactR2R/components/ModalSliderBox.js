8af5a4c53e03a690f92cfd711c8f51de415c8e92
ChrisLyndon-A1694039 <a1694039@student.adelaide.edu.au>
2017-11-15 16:55:32 +1030

    Created basic modal square from Sketch design and implemented touch info

--- React Native/ReactR2R/components/ModalSliderBox.js
+++ React Native/ReactR2R/components/ModalSliderBox.js
@@ -3,36 +3,85 @@ import {
   StyleSheet,
   View,
   Text,
+  Image,
+  PanResponder,
+  TouchableWithoutFeedback
 } from 'react-native';
+
+const getDirection = ({ moveX, moveY, dx, dy}) => {
+  const draggedDown = dy > 30;
+  const draggedUp = dy < -30;
+  const draggedLeft = dx < -30;
+  const draggedRight = dx > 30;
+  let dragDirection = '';
+
+  if (draggedDown || draggedUp) {
+    if (draggedDown) dragDirection += 'dragged down '
+    if (draggedUp) dragDirection +=  'dragged up ';
+  }
+
+  if (draggedLeft || draggedRight) {
+    if (draggedLeft) dragDirection += 'dragged left '
+    if (draggedRight) dragDirection +=  'dragged right ';
+  }
+
+  if (dragDirection) return dragDirection;
+}
 
 class ModalSliderBox extends React.Component {
   constructor(props) {
     super(props)
 
-    this.attr = {
+    this.state = {
       type: props.type,
       weather: props.weather,
       transit: props.transit,
-      test: 'blarg'
+      feedback: 'start'
     };
 
-    if (this.attr.type == 'location') {
-      this.attr.test = 'fucked up'
+    if (this.state.type == 'location') {
+      this.state.feedback = 'feedback'
     }
 
-    if (this.attr.weather == 'transit') {
+    if (this.state.weather == 'transit') {
 
     }
 
-    if (this.attr.transit == 'location') {
+    if (this.state.transit == 'location') {
 
     }
+
+    this.onPress = this.onPress.bind(this);
+  }
+
+  onPress() {
+    this.setState({
+      feedback: "Tap"
+    })
+  }
+
+  componentWillMount() {
+    this._panResponder = PanResponder.create({
+      onMoveShouldSetPanResponder:(evt, gestureState) => !!getDirection(gestureState),
+      onPanResponderMove: (evt, gestureState) => {
+        const drag = getDirection(gestureState);
+        this.setState({
+          feedback: drag,
+        })
+      },
+      onPanResponderTerminationRequest: (evt, gestureState) => true,
+    });
   }
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.nobg}>{this.attr.type}</Text>
+      <View {...this._panResponder.panHandlers}> 
+        <TouchableWithoutFeedback onPress={this.onPress}>
+          <View style={styles.container}>
+            <Image source={require('../Design/Users/Resources/Rectangle.png')} style={styles.bg}></Image>
+            <Text style={styles.nobg}>{this.state.feedback}</Text>
+          </View>
+        </TouchableWithoutFeedback>
       </View>
     );
   }
@@ -46,10 +95,20 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.4,
     shadowRadius: 20,
-    borderRadius: 20
+    borderRadius: 20,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
   },
   nobg: {
     backgroundColor: 'rgba(0,0,0,0)'
+  },
+  bg: {
+    flex: 1,
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center'
   }
 })
 
